7678328c25e0956412f1b73eb7d0aab92778a8d0
Ivan Baryutin <vaness97@gmail.com>
2021-12-28 19:10:27 +0300

    Fix: проверяем ошибки обработки данных после запросов

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -43,7 +43,7 @@ function App() {
       // api.getInitialCards(),
     ])
       .then(([userData, initialCardsData]) => {
-        console.log(2);
+        console.log(userData.name);
         setCurrentUser(userData);
         //setCards(initialCardsData);
       })
