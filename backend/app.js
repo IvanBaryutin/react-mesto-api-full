2ccd5282a678233d9996aa00dc0bf54896a5a8bb
Ivan Baryutin <vaness97@gmail.com>
2021-12-29 19:39:42 +0300

    Add: пытаемся вывести env-переменные

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 console.log(process.env.NODE_ENV); // production
+console.log(process.env.JWT_SECRET);
 const express = require('express');
 
 const mongoose = require('mongoose');
